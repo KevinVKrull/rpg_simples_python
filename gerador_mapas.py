6ae5ddb2ebb40e86375d19a12c7d73b815891161
Kevin Krull <kevinvkrull@gmail.com>
2025-11-17 21:30:40 -0300

    terminando luta

--- gerador_mapas.py
+++ gerador_mapas.py
@@ -66,11 +66,6 @@ def salas_resolvidas():
 def enfrentar_boss():
     print('')
 
-# ---------- ENFRENTAR GUARDIAO ---------------------
-def enfrentar_guardiao(mago_do_tempo, guardiao_de_pedra):
-    while True:
-        print('1')
-
 # ---------- SALA DESCANSO ---------------------
 def sala_descanso():
     print('')
